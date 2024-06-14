01c0f7ddfc1ce5be37555fa2738e533779d430c3
Raymond Lei(雷菩宇) <ray810815@gmail.com>
2024-06-13 23:55:28 -0400

    Add a HtmlDocument UnitTest Project

---
+++ HtmlDocoumentTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace HtmlDocoumentTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
